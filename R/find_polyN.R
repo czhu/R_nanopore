16b58dce70621708bd481c3472921a511ce89df4
Chenchen Zhu <isczhu@gmail.com>
2017-07-30 20:25:05 +0200

    fix reduce conflict with other package

--- R/find_polyN.R
+++ R/find_polyN.R
@@ -1,4 +1,4 @@
 ## extract polyA or polyT
 find_polyN = function(x,polyN=paste(rep("A",10),collapse="")){
-    reduce(ranges(matchPattern(polyN,x,max.mismatch=1, with.indels =TRUE)))
+    GenomicRanges::reduce(ranges(matchPattern(polyN,x,max.mismatch=1, with.indels =TRUE)))
 }
