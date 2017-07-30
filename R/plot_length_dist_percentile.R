5b22fd6d365b6fd72be2041b6ff6eb30e4b0a9aa
Chenchen Zhu <isczhu@gmail.com>
2017-07-30 20:27:38 +0200

    view

--- R/plot_length_dist_percentile.R
+++ R/plot_length_dist_percentile.R
@@ -8,7 +8,7 @@ plot_length_dist_percentile = function(ss,...){
 
     breakPoints = c(seq(0,highPoint,by=stepSize),max(ss$sequence_length_template))
     lbp = length(breakPoints)
-    mylabels = c(paste0("<",breakPoints[1]),paste(breakPoints[-c(1,lbp-1,lbp)],
+    mylabels = c(paste0("<",breakPoints[2]),paste(breakPoints[-c(1,lbp-1,lbp)],
         breakPoints[-c(1:2,lbp)],sep="-"), paste0(">",breakPoints[lbp-1]))
 
     xat = barplot(
@@ -18,9 +18,9 @@ plot_length_dist_percentile = function(ss,...){
                 labels=mylabels
             )
         )/nrow(ss),
-        xaxt="n",
-        xlab="Bins", main=paste0("Percentile of reads per bin\n","total ",nrow(ss)),las=2,cex.names=0.5,
-        xpd=TRUE,...)
+        #xaxt="n",xpd=TRUE
+        xlab="Bins", main=paste0("Percentile of reads per bin\n","total ",nrow(ss)),las=2,
+        cex.names=0.65,...)
 
     #axis(1,xat,mylabels,srt=45,cex=0.5,xpd=TRUE)
 }
