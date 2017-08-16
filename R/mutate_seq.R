08cea17f16f21935e5a6579ef211281e6202596c
Chenchen Zhu <isczhu@gmail.com>
2017-08-16 01:59:39 -0700

    muate sequence with error profiling of 1d reads

---
+++ R/mutate_seq.R
@@ -0,0 +1,16 @@
+## mutate sequencing with given error probablity
+## limitation, only 1 nt insertion allowed
+mutate_seq = function(x, substitutionRate= 0.07,insertionRate=0.03,deletionRate=0.05) {
+    require(Biostrings)
+
+    chars <- strsplit(as.character(x), "")[[1]]
+    nChars = length(chars)
+
+    rv = ifelse(rbinom(nChars,1,substitutionRate+deletionRate),
+       sample(c(DNA_BASES,""), nChars, replace = TRUE,
+            prob=c(rep(substitutionRate/4,4),deletionRate)), chars)
+    ## n + 1 position can be inserted
+    toInsert = sample(c("",DNA_BASES), nChars+1, replace=TRUE,
+        prob=c(1-insertionRate,rep(insertionRate/4,4)))
+    paste(apply(cbind(c("",rv),toInsert),1,paste0), collapse="")
+}
