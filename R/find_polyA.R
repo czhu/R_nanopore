acbe44ed8cf07654705a3aac2fd9d6f14602f48e
Chenchen Zhu <isczhu@gmail.com>
2017-08-17 20:34:51 -0700

    two modes of finding best hit

--- R/find_polyA.R
+++ R/find_polyA.R
@@ -1,11 +1,14 @@
-find_polyA = function(myseqs, n=12, max.mismatch=1L, use.names=TRUE) {
+find_polyA = function(myseqs, n=12, max.mismatch=1L,hitBy="length",use.names=TRUE) {
+    ## hitBy either by length (longest polyA/T is the hit)
+    ## or distance to the end (shortest to the end is the hit)
+
     ## myseqs shortread class
     ## n number of polyA/T
     mypaste = function(y) paste(y,collapse=",")
 
     adapters = DNAStringSet(c(
-        polyA=paste(rep("A",12),collapse=""),
-        polyT=paste(rep("T",12),collapse=""))
+        polyA=paste(rep("A",n),collapse=""),
+        polyT=paste(rep("T",n),collapse=""))
         )
 
     res = lapply(1:length(adapters), function(i) {
@@ -18,12 +21,25 @@ find_polyA = function(myseqs, n=12, max.mismatch=1L, use.names=TRUE) {
     res = res %>% arrange(group,start)
     if(use.names) res$adapter_id = names(adapters)[res$adapter_id]
 
-    res %>% group_by(group) %>% summarise(
-        hit = which.max(width),
-        allStart=mypaste(start),allEnd=mypaste(end),allWidth=mypaste(width),
-        allAdapter_id = mypaste(adapter_id),
-        start = start[hit],
-        end = end[hit],
-        adapter_id = adapter_id[hit]
-        )
+    if(hitBy=="length"){
+        rv = res %>% group_by(group) %>% summarise(
+            hit = which.max(width),
+            start = start[hit],
+            end = end[hit],
+            allStart=mypaste(start),allEnd=mypaste(end),allWidth=mypaste(width),
+            allAdapter_id = mypaste(adapter_id),
+            adapter_id = adapter_id[hit]
+            )
+    } else if (hitBy =="distanceToEnd"){
+        res$read_length = width(myseqs)[res$group]
+        rv = res %>% group_by(group) %>% summarise(
+            hit = which.min(pmin(start,read_length-end)),
+            start = start[hit],
+            end = end[hit],
+            allStart=mypaste(start),allEnd=mypaste(end),allWidth=mypaste(width),
+            allAdapter_id = mypaste(adapter_id),
+            adapter_id = adapter_id[hit]
+            )
+    }
+    rv
 }
