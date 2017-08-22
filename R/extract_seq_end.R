0d472039741e69a5290849a888bd88da40f449b0
Chenchen Zhu <isczhu@gmail.com>
2017-08-21 23:57:40 -0700

    remove extra comma

--- R/extract_seq_end.R
+++ R/extract_seq_end.R
@@ -1,7 +1,7 @@
 extract_seq_end = function(x,windowSize=200,fromEnd=FALSE){
     ## x is ShortRead class
     if(fromEnd) {
-        mystart = ifelse(width(x) - windowSize +1  <1, 1, width(x) - windowSize + 1),
+        mystart = ifelse(width(x) - windowSize +1  <1, 1, width(x) - windowSize + 1)
         myend = width(x)
     } else {
         mystart = 1
