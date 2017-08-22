8b3e433b23ce48bc794ff33a0a21f1d57c23ac8f
Chenchen Zhu <isczhu@gmail.com>
2017-08-21 23:56:40 -0700

    ifelse needs vector -> use normal if

--- R/extract_seq_end.R
+++ R/extract_seq_end.R
@@ -1,9 +1,11 @@
 extract_seq_end = function(x,windowSize=200,fromEnd=FALSE){
     ## x is ShortRead class
-    mystart = ifelse(fromEnd,
-            ifelse(width(x) - windowSize +1  <1, 1, width(x) - windowSize + 1),
-            1)
-    myend = ifelse(fromEnd,
-        width(x), ifelse(windowSize>width(x),width(x),windowSize))
+    if(fromEnd) {
+        mystart = ifelse(width(x) - windowSize +1  <1, 1, width(x) - windowSize + 1),
+        myend = width(x)
+    } else {
+        mystart = 1
+        myend = ifelse(windowSize>width(x),width(x),windowSize))
+    }
     narrow(x,mystart,myend)
 }
