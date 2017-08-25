a44fb6b6c172119559aebc180588ad3f90e1c778
Chenchen Zhu <isczhu@gmail.com>
2017-08-25 16:38:14 -0700

    fix comment

--- R/calc_mean_qscore.R
+++ R/calc_mean_qscore.R
@@ -1,6 +1,6 @@
 calc_mean_qscore = function(x) {
-    #https://community.nanoporetech.com/posts/difference-in-quality-scor
-    # qscore is in log, mean of qscores doesn't work
-    # mean of error rate works -> first scale back and take mean and taking log again
+    ## https://community.nanoporetech.com/posts/difference-in-quality-scor
+    ## qscore is in log, mean of qscores doesn't work
+    ## mean of error rate works  first scale back and take mean and taking log again
     log10(mean(10^(x/(-10))))*(-10)
 }
