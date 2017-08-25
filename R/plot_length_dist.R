ebaf667a135efa0af3594100aec5e2d1a43645cc
Chenchen Zhu <isczhu@gmail.com>
2017-08-25 14:47:47 -0700

    xlim fixed

--- R/plot_length_dist.R
+++ R/plot_length_dist.R
@@ -19,12 +19,12 @@ plot_length_dist = function(ss,main=""){
       #geom_density(color="steelblue", fill="steelblue", alpha=0.8,na.rm=TRUE) +
       geom_histogram(color="steelblue", fill="steelblue", alpha=0.8,na.rm=TRUE,bins=100) +
       #stat_density(aes(y=..count..), color="steelblue", fill="steelblue", alpha=0.8,na.rm=TRUE, n=1000) +
-      scale_x_continuous(breaks=breaks,trans="log10") +
+      scale_x_continuous(breaks=breaks,trans="log10",limits=c(lowEnd,highEnd)) +
       #scale_y_continuous(breaks=c(0,125,250,375,500,625,750), expand=c(0,0)) +
       theme_bw() + ggtitle(main) +  labs(x = "Read length in BP") +
       theme(axis.text.x = element_text(angle = 45,hjust = 1), plot.title = element_text(hjust = 0.5))+
       #geom_vline(xintercept = c(x.p25.log), color = "chocolate3", alpha = 1) +
-      geom_vline(xintercept = c(x.median.log), color = "firebrick", alpha = 1) + xlim(c(lowEnd,highEnd))
+      geom_vline(xintercept = c(x.median.log), color = "firebrick", alpha = 1)
       #geom_vline(xintercept = c(x.mean.log), color = "royalblue", alpha = 1) +
       #geom_vline(xintercept = c(x.p75.log), color = "chocolate3", alpha = 1) +
       # geom_text(x = x.p25.log, y = 200, label = paste("P25", round(x.p25.log, digits = 2), sep = '\n'), color = "chocolate3") +
