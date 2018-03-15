2e6ce7e33521882159f6c0e5b6127c11137f8b2b
Chenchen Zhu <czhu@embl.de>
2018-03-15 16:13:01 -0700

    change a few para

--- R/plot_mean_qscore_dist.R
+++ R/plot_mean_qscore_dist.R
@@ -6,13 +6,11 @@ plot_mean_qscore_dist = function(ss,main=""){
     #x.p75.log <- log10(quantile(dd$mean_qscore_template, 0.75))
 
     ggplot(ss, aes(x=mean_qscore_template)) +
-      geom_histogram(color="firebrick", fill="firebrick", alpha=0.8,na.rm=TRUE,bins=100)+
+      geom_histogram(color="firebrick", fill="firebrick", alpha=0.6,na.rm=TRUE,bins=100)+
       #stat_density(aes(y=..count..), color="black", fill="steelblue", alpha=0.5,na.rm=TRUE) +
       scale_x_continuous() +
       #scale_y_continuous(breaks=c(0,125,250,375,500,625,750), expand=c(0,0)) +
-      theme_bw() + ggtitle(main) + labs(x = "Mean quality score per read") +
+      theme_bw() + ggtitle(main) + labs(x = "Mean quality score per read", y = "Count") +
       theme(axis.text.x = element_text(angle = 45,hjust = 1),plot.title = element_text(hjust = 0.5))+
-
-       geom_vline(xintercept = c(x.median), color = "red", alpha = 1)
-
+      geom_vline(xintercept = c(x.median), color = "darkgreen", alpha = 1)
 }
