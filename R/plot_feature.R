c461a4131a4212b2b042c59c61f055d3d202165f
Chenchen Zhu <czhu@me.com>
2018-09-28 19:12:59 -0700

    add just to pllot_feature_text and set xjust and yjust to NULL

--- R/plot_feature.R
+++ R/plot_feature.R
@@ -167,7 +167,7 @@ plot_feature  = function(x, coord, lineWidth, featureCols="steelblue", featureAl
 # pushViewport(dataViewport(xData=c(10,100), yscale=c(0,1), extension=0, clip="off"))
 # grid.rect()
 # plot_feature_text(x,mytext,fontsize=20,debug=TRUE,plotBottomToTop=FALSE)
-plot_feature_text = function(x,text,fontsize=12,side=0, col="black",xjust=unit(0,"npc"), yjust=unit(0,"npc"), plotBottomToTop=TRUE,debug=FALSE){
+plot_feature_text = function(x, text, fontsize=12, side=0, col="black",just="center", xjust=NULL, yjust=NULL, plotBottomToTop=TRUE, debug=FALSE){
     ## side: 0 center,1, left, 2,top 3,right 4 bottom
     ## xjust, yjust, shift in x or y if defined, currently ignored
     ## textSize: text height in points
@@ -200,12 +200,11 @@ plot_feature_text = function(x,text,fontsize=12,side=0, col="black",xjust=unit(0
         #ytext = myy + featureHeight*1.5
     }
 
-    grid.text(text, x =unit(xtext,"native"), y = unit(ytext,"points"),just="center",
+    grid.text(text, x =unit(xtext,"native"), y = unit(ytext,"points"),just=just,
         hjust = xjust, vjust = yjust, gp = gpar(col=col,fontsize=fontsize))
 }
 
-plot_feature_text_vpr  = function(x, text, vpr,coord, fontsize=12,side=0, col="black",xjust=unit(0,"npc"), yjust=unit(0,"npc"),
-    plotBottomToTop=TRUE,debug=FALSE) {
+plot_feature_text_vpr  = function(x, text, vpr,coord, fontsize=12,side=0, col="black", just = "center", xjust=NULL, yjust=NULL, plotBottomToTop=TRUE, debug=FALSE) {
     ## x is a GRanges object with blocks
     ## conivence functon to call plot_feature with vpr
     if(missing(vpr)) {
@@ -217,7 +216,7 @@ plot_feature_text_vpr  = function(x, text, vpr,coord, fontsize=12,side=0, col="b
     pushViewport(
         dataViewport(xData=coord, yscale=c(0,1), extension=0, clip="off",
         layout.pos.col=1,layout.pos.row=vpr))
-    plot_feature_text(x=x,text=text,fontsize=fontsize,side=side, col=col,xjust=xjust, yjust=yjust,
+    plot_feature_text(x=x,text=text,fontsize=fontsize,side=side, col=col, just=just, xjust=xjust, yjust=yjust,
             plotBottomToTop=plotBottomToTop,debug=debug)
     popViewport()
 }
