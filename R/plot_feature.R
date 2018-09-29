e57964ad4ceb984e23b5820fb3585139051b1834
Chenchen Zhu <czhu@me.com>
2018-09-28 18:53:52 -0700

    plot_feature_text with vjust and hjust

--- R/plot_feature.R
+++ R/plot_feature.R
@@ -167,8 +167,7 @@ plot_feature  = function(x, coord, lineWidth, featureCols="steelblue", featureAl
 # pushViewport(dataViewport(xData=c(10,100), yscale=c(0,1), extension=0, clip="off"))
 # grid.rect()
 # plot_feature_text(x,mytext,fontsize=20,debug=TRUE,plotBottomToTop=FALSE)
-plot_feature_text = function(x,text,fontsize=12,side=0, col="black",xjust=unit(0,"npc"), yjust=y(0,"npc"),
-    plotBottomToTop=TRUE,debug=FALSE){
+plot_feature_text = function(x,text,fontsize=12,side=0, col="black",xjust=unit(0,"npc"), yjust=unit(0,"npc"), plotBottomToTop=TRUE,debug=FALSE){
     ## side: 0 center,1, left, 2,top 3,right 4 bottom
     ## xjust, yjust, shift in x or y if defined, currently ignored
     ## textSize: text height in points
@@ -200,7 +199,8 @@ plot_feature_text = function(x,text,fontsize=12,side=0, col="black",xjust=unit(0
         #ytext = myy + featureHeight*1.5
     }
 
-    grid.text(text, x =unit(xtext,"native"), y = unit(ytext,"points"),just="center", gp = gpar(col=col,fontsize=fontsize))
+    grid.text(text, x =unit(xtext,"native"), y = unit(ytext,"points"),just="center",
+        hjust = xjust, vjust = yjust, gp = gpar(col=col,fontsize=fontsize))
 }
 
 plot_feature_text_vpr  = function(x, text, vpr,coord, fontsize=12,side=0, col="black",xjust=unit(0,"npc"), yjust=y(0,"npc"),
