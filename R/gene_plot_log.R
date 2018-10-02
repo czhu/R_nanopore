0a549b6604c0fd597afe58722b261b0b8bcace8e
Chenchen Zhu <czhu@me.com>
2018-10-01 19:05:39 -0700

    add consensusTrackHeight

--- R/gene_plot_log.R
+++ R/gene_plot_log.R
@@ -5,8 +5,8 @@
 ## be refact
 
 gene_plot_log = function(GeneModel, txConsensus, txReads, txHighlight,
-    title, plotTopToBottom=TRUE, axisHeight= 20, consensusTrackHeight = 5,
-    consensusFeatureHeight = 3, trackHeights, readHeight) {
+    title, plotTopToBottom=TRUE, axisHeight= 20, annotationTrackHeight=2,
+    consensusTrackHeight = 5,consensusFeatureHeight = 3, trackHeights, readHeight) {
 
     myfunc = identity
     isPlusStrand = as.logical(unique(strand(txConsensus)) == "+")
@@ -29,6 +29,6 @@ gene_plot_log = function(GeneModel, txConsensus, txReads, txHighlight,
     ## TODO change gene mode track height
     chrom_plot(plotDat,coord=c(start(coord), end(coord)),
         plotCountNum=TRUE,debug=FALSE, featureHeightPerRead = consensusFeatureHeight,
-        config=generate_plot_config(list(readConsensus=list(height=consensusTrackHeight), geneModel=list(height=5))),
+        config=generate_plot_config(list(readConsensus=list(height=consensusTrackHeight), geneModel=list(height=5))), annotationTrackHeight = annotationTrackHeight,
         spaceBetweenCluster = 4, shiftLabel=TRUE,geneNameTrackHeight=0, geneTrackHeight=0, singleStrand=TRUE, consensusNameFontsize = 5, highlightFontsize=5, genomeAxisHeight=axisHeight)
 }
